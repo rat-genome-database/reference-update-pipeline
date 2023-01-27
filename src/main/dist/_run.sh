acce95dd5c7f0d26ab2730a13786ff0d67318999
tutajm <mtutaj@mcw.edu>
2023-01-27 09:23:37 -0600

    upgraded commons-dbcp

--- src/main/dist/_run.sh
+++ src/main/dist/_run.sh
@@ -6,7 +6,7 @@ APPDIR=/home/rgddata/pipelines/$APPNAME
 
 cd $APPDIR
 
-java -Dspring.config=$APPDIR/../properties/default_db.xml \
+java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/${APPNAME}.jar "$@" > run.log 2>&1
 
